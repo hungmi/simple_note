c2076d3a69a989153b3b0a8e158f7a2c52e8b325
hungmi <hungmihungmi@gmail.com>
2019-08-26 13:40:26 +0800

    add seed for initialization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+unless (@user = User.first).present?
+	@user = User.create(name: "test_user", password: "123456")
+end
+
+@first_notebook = Notebook.create({
+	name: "生活賬本"
+})
+
+10.times do |i|
+	@first_notebook.payments.create({
+		total: rand(100..10000),
+		recorder: @user
+	})
+end
