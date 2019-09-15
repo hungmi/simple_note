f3bf8f5bbde270b9a2a61ee35110579c07afcb99
hungmi <hungmihungmi@gmail.com>
2019-09-15 14:18:52 +0800

    fix 換頁時導覽列會彈一下的觀感

--- app/assets/javascripts/admin.js
+++
@@ -1,2 +0,0 @@
-//= require popper
-//= require tablesort.number.min
--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -3,8 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap/dist/js/bootstrap'
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
