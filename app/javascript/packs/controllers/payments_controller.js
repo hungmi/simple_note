12210d2d75cbe5a1f425103512ea36842ad829b9
hungmi <hungmihungmi@gmail.com>
2020-01-24 00:35:26 +0800

    monkey patch 換頁 input 功能

--- app/javascript/packs/controllers/payments_controller.js
+++ app/javascript/packs/controllers/payments_controller.js
@@ -7,6 +7,21 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 	static targets = [ "minDate", "maxDate" ]
+	connect() {
+		document.querySelector("#js-admin-payments-pagy input[type='number']").addEventListener("change", function(e) {
+			if (parseInt(e.target.value) > 0) {
+				if (window.location.href.indexOf("page") > -1) {
+					window.location.href = window.location.href.replace(/(page=)\d+/, `$1${e.target.value}`)
+				} else {
+					if (window.location.href.indexOf("?") > -1) {
+						window.location.href += `&page=${e.target.value}`
+					} else {
+						window.location.href += `?page=${e.target.value}`
+					}
+				}
+			}
+		})
+	}
   	setDateFilter(e) {
   		var controller = this
   		this.minDateTarget.value = e.target.value
